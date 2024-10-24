55306dd4aa8d266615ac658b4cacd3fe2683abbb
Vivian <vivlopes95@gmail.com>
2024-10-24 21:27:05 +0100

    created function to get api endpoint and endpoint route to handle logic for quiz questions

--- server.js
+++ server.js
@@ -1,26 +1,59 @@
-// imports 
+//IMPORTS 
 import "dotenv/config"
 import express from "express"
 import cors from "cors"
 import axios from "axios"
 
 
-//Global Variables
+//GLOBAL VARIABLES
 const app = express()
-const PORT = process.env.Port || 5000
-const COUNTRT_API_URL = Process.env.COUNTRT_API_URL
+const PORT = process.env.PORT
+const COUNTRY_API_URL = process.env.COUNTRY_API_URL
 
 app.use(cors())
 app.use(express.json())
 
-// Variables
+// VARIABLES
 let countriesArr = []
 
-// Functions
+// FUNCTIONS
+// function to get countries data from API
+async function getCountries() {
+    try {
+        const res = await axios.get(COUNTRY_API_URL);
 
+        // filter out invalid options
+        const validCountries = res.data.data.filter(country => {
+            return country.capital && country.capital !== ""
+        })
+        console.log(validCountries)
+        countriesArr = validCountries
+        return countriesArr;
+    } catch (error) {
+        console.error('Error fetching countries:', error);
+        throw error;
+    }
+}
 
-// Routes
+// ROUTES
+// create endpoint to get the quiz data
+app.get('/quiz', async (req, res) => {
+    try {
+        await getCountries()
+        // select a random counrty
+        const randIndex = Math.floor(Math.random() * countriesArr.length)
+        // save to a variable
+        const correctCountry = countriesArr[randIndex]
+        console.log(correctCountry)
+    } 
+    
+    catch (error) {
+        console.error('Error fetching countries:', error);
+        res.status(500).send('Error fetching countries');
+    }
+})
 
+// endpoint to confirm port is running
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
